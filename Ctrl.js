8257c2eb5ace8005a4f7f64804d48107878e69b0
tjdalsvndn9 <ssuyi@ucdavis.edu>
2016-12-13 16:57:49 -0800

    first commit

---
+++ Ctrl.js
@@ -0,0 +1,73 @@
+app.controller('oneController', function($scope,$timeout) {
+	var main = this;
+	main.title ="Provide the following words: ";
+	main.header = "Automate This!";
+	//form submit
+	$scope.submit = submit;
+
+	$scope.switchProblem= switchProblem;
+	//data example
+  $scope.showExample = showExample;
+  //reset button
+ $scope.doitAgain = doitAgain;
+  //single-side application
+ $scope.Active = true;
+
+ function submit(){
+if($scope.myForm.$invalid){
+$scope.Active = true;
+
+
+}
+else if($scope.myForm.$valid){
+
+	console.log("it is submitted");
+}
+ }
+
+
+ function showExample(){
+ 	$scope.character={
+gender : "male",
+name : "Sung Yi",
+jobTitle : "Web Developer",
+tediousTask : "coding",
+dirtyTask : "coding",
+obnoxiousCelebrity : "you",
+celebrity : "boss",
+hugeNumber : 100,
+uselessSkill : "coding",
+adjective : "hefty"
+ 	}
+ 	
+
+	return $timeout(function(){
+	$scope.character = {};
+
+	},4000);
+ }
+ 
+ function switchProblem(){
+ 	if($scope.gender === "male"){
+ 		$scope.pronoun = "he";
+ 		$scope.pronoun2 = "his";
+ 	}
+ 	else{
+ 	$scope.pronoun = "she";
+ 		$scope.pronoun2 = "her";	
+ 	}
+
+
+ 	$scope.Active = false;
+
+
+
+ }
+function doitAgain(){
+	input = angular.element(document.getElementsByTagName("INPUT"));
+	console.log("Reset!");
+	$scope.character= {};
+	$scope.Active = true;
+}
+
+});
